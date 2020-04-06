438935ce166aecfe42f0e5df548821d6cd28d784
stgrv64 <stgrv64@gmail.com>
2020-04-06 14:38:17 +0200

    ajout repertoire lirc.analyse (faite sur astrokit prod)

---
+++ lirc.analyse/ls.etc.lirc.lircd.conf.d
@@ -0,0 +1,13 @@
+total 88
+-rw-r--r--    1 root     root         17854 Feb  1 14:22 01-gowell-1.conf
+-rw-r--r--    1 root     root          7205 Feb  1 14:22 01-gowell-2.conf
+-rw-r--r--    1 root     root         17854 Feb  1 14:22 01-gowell.conf
+drwxr-xr-x    2 root     root          1024 Feb  1 14:22 ENTETE.CONF
+-rw-r--r--    1 root     root          1628 Feb  1 14:22 KEYS_code_1120.txt
+drwxr-xr-x    2 root     root          1024 Feb  1 14:22 OLD
+-rw-r--r--    1 root     root           935 Feb  1 14:22 README.conf.d
+-rw-r--r--    1 root     root          1901 Feb 21 21:05 code_1120.conf
+-rw-r--r--    1 root     root          1527 Feb  1 14:22 irrecord.conf
+-rw-r--r--    1 root     root             0 Feb  1 14:22 irrecord.conf.conf
+-rw-r--r--    1 root     root         19898 Feb  1 14:22 meliconi.conf
+-rw-r--r--    1 root     root         14448 Feb  1 14:22 mini.conf
