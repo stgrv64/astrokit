9f6e9d28a8ea2a563435bc81e3c9ecfb70bbcaf3
Stephane Gravois <stef@astrokit-4.14.98-armv7l-rpi3a>
2021-11-14 16:59:32 +0100

    creation repertoire gpio-ir + MAJ fichiers - procedure dans config-raspbian-rpi3a+

---
+++ gpio-ir/config-raspbian-rpi3a+/copier-ici-modules.gpio-ir-recv.sh
@@ -0,0 +1,1 @@
+sudo cp -p  /lib/modules/4.19.66-v7+/kernel/drivers/media/rc/gpio-ir-recv.ko .
---
+++ gpio-ir/config-raspbian-rpi3a+/insert.modules.gpio-ir.sh
@@ -0,0 +1,1 @@
+sudo insmod  /lib/modules/4.19.66-v7+/kernel/drivers/media/rc/gpio-ir-recv.ko gpio_pin=27
